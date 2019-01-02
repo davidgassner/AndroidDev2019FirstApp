b45b76f63494792bf5efe6f8bc523598fbaaba5c
David Gassner <dgassnerdev@gmail.com>
2019-01-02 11:17:27 -0800

    04_07

---
+++ app/src/main/java/com/example/myfirstapp/DetailActivity.kt
@@ -0,0 +1,17 @@
+package com.example.myfirstapp
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+
+import kotlinx.android.synthetic.main.activity_detail.*
+import kotlinx.android.synthetic.main.content_detail.*
+
+class DetailActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_detail)
+        setSupportActionBar(toolbar)
+    }
+
+}
