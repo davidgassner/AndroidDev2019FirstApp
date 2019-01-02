51bffea398a63003e25534792e32f50bd879ce9b
David Gassner <dgassnerdev@gmail.com>
2019-01-02 10:33:37 -0800

    04_06e

---
+++ app/src/main/java/com/example/myfirstapp/Main2Activity.kt
@@ -0,0 +1,23 @@
+package com.example.myfirstapp
+
+import android.content.Intent
+import android.os.Bundle
+import com.google.android.material.snackbar.Snackbar
+import androidx.appcompat.app.AppCompatActivity;
+
+import kotlinx.android.synthetic.main.activity_main2.*
+
+class Main2Activity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main2)
+        setSupportActionBar(toolbar)
+
+        fab.setOnClickListener { view ->
+            val intent = Intent(this, MainActivity::class.java)
+            startActivity(intent)
+        }
+    }
+
+}
