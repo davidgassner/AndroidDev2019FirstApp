043ef9b0a6b59150ad09bdf4f31f2eef9b0d5d4c
David Gassner <dgassnerdev@gmail.com>
2019-01-02 11:41:15 -0800

    04_07e

--- app/src/main/java/com/example/myfirstapp/DetailActivity.kt
+++ app/src/main/java/com/example/myfirstapp/DetailActivity.kt
@@ -1,6 +1,7 @@
 package com.example.myfirstapp
 
 import android.os.Bundle
+import android.util.Log
 import androidx.appcompat.app.AppCompatActivity
 
 import kotlinx.android.synthetic.main.activity_detail.*
@@ -12,6 +13,8 @@ class DetailActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_detail)
         setSupportActionBar(toolbar)
+
+        Log.i("Intents", "Action = ${intent.action}")
     }
 
 }
--- app/src/main/java/com/example/myfirstapp/MainActivity.kt
+++ app/src/main/java/com/example/myfirstapp/MainActivity.kt
@@ -1,8 +1,10 @@
 package com.example.myfirstapp
 
+import android.content.Intent
 import android.os.Bundle
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import kotlinx.android.synthetic.main.content_main.*
 
 class MainActivity : AppCompatActivity() {
 
@@ -10,6 +12,14 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(toolbar)
+
+        monster01.setOnClickListener{viewMonsterImage(R.drawable.monster01)}
+    }
+
+    private fun viewMonsterImage(monsterId: Int) {
+        val intent = Intent(this, DetailActivity::class.java)
+        intent.setAction(Intent.ACTION_VIEW)
+        startActivity(intent)
     }
 
 }
